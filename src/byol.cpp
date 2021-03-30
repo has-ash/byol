cb728d8c62434f34c7e8374b182721b3a7713f44
Hasan Ashraf <hasan_ashraf@fastmail.com>
2021-03-30 14:24:40 +0200

    added error handling

--- src/byol.cpp
+++ src/byol.cpp
@@ -2,49 +2,111 @@
 #include <cstring>
 #include <iostream>
 
+using std::cout;
+
 extern "C" {
 #include "mpc.h"
 #include <editline/readline.h>
 }
 
-long eval_op(long x, char *op, long y) {
-  if (std::strcmp(op, "+") == 0) {
-    return x + y;
+// TODO: consider using std::variant instead of lval struct.
+// Seems to make a decent amount of sense to use that instead.
+
+// for the lval.type field
+enum class lval_type { LVAL_NUM, LVAL_ERR };
+// for the lval.err field; LERR_NOP is a dummy value for creating
+// structs that have type==lval_type::LVAL_NUM i.e. it indicates
+// that the struct does not repsent an error.
+enum class err_type { LERR_NOP, LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// lval: lisp value (either valid value or error)
+// what is the difference between type and err?
+// type==0: valid number, type==1: error
+struct lval {
+  lval_type type;
+  long num;
+  err_type err;
+
+  friend std::ostream &operator<<(std::ostream &os, lval to_print) {
+    switch (to_print.type) {
+    case lval_type::LVAL_NUM:
+      os << to_print.num;
+      break;
+
+    case lval_type::LVAL_ERR:
+      if (to_print.err == err_type::LERR_DIV_ZERO) {
+        os << "Error: Division by zero!";
+      } else if (to_print.err == err_type::LERR_BAD_OP) {
+        os << "Error: Invalid Operator";
+      } else if (to_print.err == err_type::LERR_BAD_NUM) {
+        os << "Error: Invalid Number!";
+      }
+      break;
+    }
+    os << "\n";
+    return os;
   }
-  if (std::strcmp(op, "*") == 0) {
-    return x * y;
-  }
-  if (std::strcmp(op, "-") == 0) {
-    return x - y;
-  }
-  if (std::strcmp(op, "/") == 0) {
-    return x / y;
-  }
-  if (std::strcmp(op, "%") == 0) {
-    return x % y;
-  }
-  if (std::strcmp(op, "^") == 0) {
-    return std::pow(x, y); // implicit conversion happens here
-  }
-  if (std::strcmp(op, "min") == 0) {
-    return x < y ? x : y;
-  }
-  if (std::strcmp(op, "max") == 0) {
-    return x > y ? x : y;
-  }
-  return 0;
+};
+
+// Perhaps refactor: would a constructor be a better idea?
+// although two constructors overloaded on long/int do not seem like a great
+// idea
+lval lval_num(long x) {
+  return lval{lval_type::LVAL_NUM, x, err_type::LERR_NOP};
 }
 
-long eval(mpc_ast_t *t) {
+lval lval_err(err_type x) { return lval{lval_type::LVAL_ERR, 0, x}; }
+
+lval eval_op(lval x, char *op, lval y) {
+  if (x.type == lval_type::LVAL_ERR) {
+    return x;
+  } else if (y.type == lval_type::LVAL_ERR) {
+    return y;
+  } else {
+
+    if (std::strcmp(op, "+") == 0) {
+      return lval_num(x.num + y.num);
+
+    } else if (std::strcmp(op, "*") == 0) {
+      return lval_num(x.num * y.num);
+
+    } else if (std::strcmp(op, "-") == 0) {
+      return lval_num(x.num - y.num);
+
+    } else if (std::strcmp(op, "/") == 0) {
+      return y.num == 0 ? lval_err(err_type::LERR_DIV_ZERO)
+                        : lval_num(x.num / y.num);
+
+    } else if (std::strcmp(op, "%") == 0) {
+      return lval_num(x.num % y.num);
+
+    } else if (std::strcmp(op, "^") == 0) {
+      return lval_num(std::pow(x.num, y.num));
+
+    } else if (std::strcmp(op, "min") == 0) {
+      return x.num < y.num ? lval_num(x.num) : lval_num(y.num);
+
+    } else if (std::strcmp(op, "max") == 0) {
+      return x.num > y.num ? lval_num(x.num) : lval_num(y.num);
+    }
+  }
+  return lval_err(err_type::LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t *t) {
   // if number then just return number
   if (std::strstr(t->tag, "number")) {
-    return std::atol(t->contents);
+    // errno is a macro that can be set by standard library functions
+    // to the error number macros defined in cerrno
+    errno = 0;
+    long x = std::strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(err_type::LERR_BAD_NUM);
   }
   // else return result of eval-ing subtree
 
   // first child is '('
   char *op = t->children[1]->contents;
-  long x = eval(t->children[2]);
+  lval x = eval(t->children[2]);
 
   int i = 3;
   while (std::strstr(t->children[i]->tag, "expr")) {
@@ -65,14 +127,14 @@ int main() {
       MPCA_LANG_DEFAULT,
       "                                                                            \
         number      : /-?[0-9]+/;                                                  \
-        operator    : '+' | '-' | '*' | '/' | '%' | '^' ;           \
+        operator    : '+' | '-' | '*' | '/' | '%' | '^' ;                          \
         expr        : <number> | '(' <operator> <expr>+ ')';                       \
         lispy       : /^/ <operator> <expr>+ /$/ ;                                 \
         ",
       Number, Operator, Expr, Lispy);
 
-  std::cout << "Mlisp Version 0.0.1\n";
-  std::cout << "Type 'exit' to exit\n";
+  cout << "Mlisp Version 0.0.1\n";
+  cout << "Type 'exit' to exit\n";
   while (true) {
     std::string input = readline("mlisp> ");
 
@@ -84,8 +146,8 @@ int main() {
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input.c_str(), Lispy, &r)) {
-      long result = eval(static_cast<mpc_ast_t *>(r.output));
-      std::cout << result << "\n";
+      lval result = eval(static_cast<mpc_ast_t *>(r.output));
+      cout << result << "\n";
       // mpc_ast_print(static_cast<mpc_ast_t *>(r.output));
       mpc_ast_delete(static_cast<mpc_ast_t *>(r.output));
     } else {
